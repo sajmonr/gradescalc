e7bb1984f3a4d14001744fd5632f94ace7ed0af9
sajmonr <a.simonicek@yahoo.ca>
2019-04-15 12:42:24 -0400

    Added test

---
+++ src/edu/kennesaw/seclass/gradescalc/Assignment.java
@@ -0,0 +1,14 @@
+package edu.kennesaw.seclass.gradescalc;
+
+public class Assignment {
+    private String _title;
+
+    public String getTitle(){
+        return _title;
+    }
+
+    public Assignment(String title){
+        _title = title;
+    }
+
+}
---
+++ src/edu/kennesaw/seclass/gradescalc/Project.java
@@ -0,0 +1,13 @@
+package edu.kennesaw.seclass.gradescalc;
+
+public class Project {
+    private String _title;
+
+    public String getTitle(){
+        return _title;
+    }
+
+    public Project(String title){
+        _title = title;
+    }
+}
